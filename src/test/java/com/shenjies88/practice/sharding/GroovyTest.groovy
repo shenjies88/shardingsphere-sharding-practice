c78ecf3c0e5ea7589a46528453d581846854c130
shenjies88 <1961486254@qq.com>
2021-02-01 17:03:39 +0800

    fix

--- src/test/java/com/shenjies88/practice/sharding/GroovyTest.groovy
+++ src/test/java/com/shenjies88/practice/sharding/GroovyTest.groovy
@@ -1,8 +1,12 @@
 package com.shenjies88.practice.sharding
 
-class Test {
-    static void main(String[] args) {
+import org.junit.jupiter.api.Test
+
+class GroovyTest {
+
+    @Test
+    void test() {
         def date = new Date().parse("yyyy-MM-dd HH:mm:ss", '2021-01-01 00:00:00')
-        println(date.getMonth()+1)
+        println(date.getMonth() + 1)
     }
 }
